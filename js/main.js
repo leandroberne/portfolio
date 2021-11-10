86fd6c3a31c227c13da44be37d1cd936bd9dbba9
Leandro Berne <leandro@plw.com.ar>
2021-11-10 10:21:13 -0300

    agregado html y css base portfolio y skillset

--- js/main.js
+++ js/main.js
@@ -25,3 +25,10 @@ const scrollFunction = () => {
     btnUp.style.display = 'none';
   }
 };
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
